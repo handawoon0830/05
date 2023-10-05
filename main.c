dae169eacfc8b61de9a432dabcca73cffa48a01f
handawoon0830 <handawoon0830@naver.com>
2023-10-05 16:36:41 +0900

    6

--- main.c
+++ main.c
@@ -4,18 +4,29 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;
-	int sum=0;
-	int i;
+	int a,b;
+	int trial;
+	a=129;
+	trial=1;
+	printf("enter the number:");
+	scanf("%d",&b);
 	
-	printf("input a number:");
-	scanf("%d",&a);
-	
-	for(i=0;i<=a;i++)
+	while (b!=a)
 	{
-		sum+=i;
-	}
-	
-	printf("%d",sum);
+		if (b>a){
+		printf("high\n");
+		printf("enter the number:");
+		scanf("%d",&b);
+		trial+=1;
+		}
+	else if (b<a){
+		printf("low\n");
+		printf("enter the number:");
+		scanf("%d",&b);
+		trial+=1;
+		}
+	};
+	printf("answer");
+	printf("congratulation trials: %d", trial);
 	return 0;
 }
