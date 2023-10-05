a84415d1bd064083bcd4653411bab69f1fbc568e
handawoon0830 <handawoon0830@naver.com>
2023-10-05 16:18:44 +0900

    3

--- main.c
+++ main.c
@@ -4,14 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;
+	int c; //char으로 잡아도 문제 없음 
+	int num;
 	
-	printf("enter the number:");
-	scanf("%d",&a);
+	num=0;
 	
-	if (a>=0)
-		printf("%d",a);
-	else if (a<0);
-		printf("%d",-a); 
+	printf("input a string:");
+	
+	while((c=getchar()) != '\n')
+	{
+		if (c>='0' && c<='9') //'0'<= c<= '9'은 문법적으로 틀림 
+			num= num+1;
+	}
+			
+	printf("%d",num);
 	return 0;
 }
